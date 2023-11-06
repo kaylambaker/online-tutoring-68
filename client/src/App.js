11a99533d5e41c1088ee5a89ea9e729af070a71e
koolname99 <72174759+koolname99@users.noreply.github.com>
2023-11-06 16:45:36 -0600

    compile error

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,36 @@
-import './App.css';
-import "./Style.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import HomePage from './pages/Home';
-import SignupPage from './pages/Signup';
-import TutorProfilePage from './pages/TutorProfile';
-import CalendarPage from './pages/MyCalendar'
-import TutorSearchPage from './pages/TutorSearch'
-import TOTPSetup from './pages/TOTPSetup'
-import TOTPVerify from './pages/TOTPVerify'
+import "./App.css";
+import "./pages/Style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import SignupPage from "./pages/Signup";
+import TutorProfilePage from "./pages/TutorProfile";
+import CalendarPage from "./pages/MyCalendar";
+import TutorSearchPage from "./pages/TutorSearch.js";
+import TOTPSetup from "./pages/TOTPSetup.js";
+import TOTPVerify from "./pages/TOTPVerify.js";
 import TutorEditProfile from "./pages/tutoredit";
-import TempLogin from './pages/tempLogin'
+import StudentEditProfile from "./pages/studentedit";
+import TempLogin from "./pages/tempLogin";
 
 function App() {
   return (
-      <div className="max-w-md w-full space-y-8">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/tutor/:id" element={<TutorProfilePage/>} />
-            <Route path="/tutor/search" element={<TutorSearchPage/>} />
-            <Route path="/calendar" element={<CalendarPage/>} />
-            <Route path="/TOTPSetup" element={<TOTPSetup/>} />
-            <Route path="/TOTPVerify" element={<TOTPVerify/>} />
+    <div className="max-w-md w-full space-y-8">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/tutor/:id" element={<TutorProfilePage />} />
+          <Route path="/tutor/search" element={<TutorSearchPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/TOTPSetup" element={<TOTPSetup />} />
+          <Route path="/TOTPVerify" element={<TOTPVerify />} />
           <Route path="/tutoredit" element={<TutorEditProfile />} />
-            <Route path="/login" element={<TempLogin/>} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+          <Route path="/studentedit" element={<StudentEditProfile />} />
+          <Route path="/login" element={<TempLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
-
