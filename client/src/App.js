375b8e0ef6c0250c4620f7af3601625a59525007
koolname99 <72174759+koolname99@users.noreply.github.com>
2023-11-26 17:24:10 -0600

    working front and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,49 @@
-import './App.css';
+import "./App.css";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import HomePage from './pages/Home';
-import SignupPage from './pages/Signup';
-import TutorProfilePage from './pages/TutorProfile';
-import CalendarPage from './pages/MyCalendar'
-import TutorSearchPage from './pages/TutorSearch'
-import TOTPSetup from './pages/TOTPSetup'
-import TOTPVerify from './pages/TOTPVerify'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import SignupPage from "./pages/Signup";
+import TutorProfilePage from "./pages/TutorProfile";
+import CalendarPage from "./pages/MyCalendar";
+import TutorSearchPage from "./pages/TutorSearch";
+import TOTPSetup from "./pages/TOTPSetup";
+import TOTPVerify from "./pages/TOTPVerify";
 import TutorEditProfile from "./pages/tutoredit";
-import Login from './pages/Login'
-import Logout from './pages/Logout'
-import FavoritesList from './pages/FavoritesList';
-import AddToFavorites from './pages/AddToFavorites';
-import StudentDashboard from './pages/StudentDashboard'
+import StudentEditProfile from "./pages/studentedit";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import FavoritesList from "./pages/FavoritesList";
+import AddToFavorites from "./pages/AddToFavorites";
+import StudentDashboard from "./pages/StudentDashboard";
 
 function App() {
   return (
-      <div className="max-w-md w-full space-y-8">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage/>} />
-            <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/tutor/:id" element={<TutorProfilePage/>} />
-            <Route path="/tutor/search" element={<TutorSearchPage/>} />
-            <Route path="/calendar" element={<CalendarPage/>} />
-            <Route path="/TOTPSetup" element={<TOTPSetup/>} />
-            <Route path="/TOTPVerify" element={<TOTPVerify/>} />
-            <Route path="/tutoredit" element={<TutorEditProfile />} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/logout" element={<Logout/>} />
-            <Route path="/students/favorites_list/:StudentID" element={<FavoritesList/>}/>
-            <Route path="/students/favorites_list/:StudentID/:TutorID" element={<AddToFavorites/>}/>
+    <div className="max-w-md w-full space-y-8">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/tutor/:id" element={<TutorProfilePage />} />
+          <Route path="/tutor/search" element={<TutorSearchPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/TOTPSetup" element={<TOTPSetup />} />
+          <Route path="/TOTPVerify" element={<TOTPVerify />} />
+          <Route path="/tutoredit" element={<TutorEditProfile />} />
+          <Route path="/studentedit" element={<StudentEditProfile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route
+            path="/students/favorites_list/:StudentID"
+            element={<FavoritesList />}
+          />
+          <Route
+            path="/students/favorites_list/:StudentID/:TutorID"
+            element={<AddToFavorites />}
+          />
           <Route path="/studentdashboard" element={<StudentDashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
